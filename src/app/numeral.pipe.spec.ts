ddf288f681f4531b0248eda2a4f541b265ab5b18
Michael Strong <mstrong@silverstripe.com>
2017-02-09 14:19:26 +1300

    Added tests for numeral pipe

---
+++ src/app/numeral.pipe.spec.ts
@@ -0,0 +1,18 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { NumeralPipe } from './numeral.pipe';
+
+describe('PositionPipe', () => {
+  it('create an instance', () => {
+    const pipe = new NumeralPipe();
+    expect(pipe).toBeTruthy();
+  });
+
+  it('should format positions with correct suffix', () => {
+    const pipe = new NumeralPipe();
+
+    let value = 1;
+    expect(pipe.transform(value, 'Oo').toEqual('1st');)
+  });
+});
